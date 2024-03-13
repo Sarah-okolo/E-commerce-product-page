847e7887d09126e435d167dac7b8a2f584675531
unknown <okolosarah402@gmail.com>
2024-03-13 16:34:26 +0100

    Initial commit: ecommerce product page project

---
+++ README.md
@@ -0,0 +1,84 @@
+# Frontend Mentor - E-commerce product page solution
+
+This is a solution to the [E-commerce product page challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the site depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Open a lightbox gallery by clicking on the large product image
+- Switch the large product image by clicking on the small thumbnail images
+- Add items to the cart
+- View the cart and remove items from it
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+
+## My process
+   
+   -I created the html file first
+   -Added the css, (designed mobile first), so i designed for the desktop view after i was done with the mobile view.
+   -created the menu functions in js for both the mobile and desktop view.
+   -created the carting functions.
+   -created the images view functions.
+   -created the lightbox for the desktop view, and i was done.
+   
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Vanilla JS
+
+
+### What I learned
+
+ *This challange helped me learn quite a few new things...
+ -I learned how to clone an existing element in the dom, into another element, and display the output.
+  you can see that in line 158 of the JavaScript file. 
+ -Was able to learn how to create a function image lightbox with vanilla js. you can also see how i implemented that in line 158 of the js file.
+ -I learned how to create an items carting function, which updates the cart according to how many items were carted, first time doing that, so i was really excited i was able to figure that out. you can check out how i implemented that from line 77 to line 128 in the js file.
+ 
+
+### Continued development
+ -I am looking forward to learn and really understand how to use Ajax for development, and i might be doing that in my next project (really excited about this journey)
+ 
+ 
+### Useful resources
+  **(https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore)
+ - This is an amazing article which helped me finally understand how to clone elements. I'd recommend it to anyone still learning this concept.
+
+
+## Author
+
+- Frontend Mentor - (https://www.frontendmentor.io/profile/sarah-okolo)
+
+
+## Acknowledgments
+
+ -I want to say a big thanks to my learning team, we took on this project together, to develop our skills, and they were of great help to me, and pointed out my errors and bad practices, so i am really grateful that i came across them.
+- And i also want to appreciate Frontend Mentor for creating such a nice challenge to sharpen my development skills, and to help me grow on the right path.
---
+++ app.js
@@ -0,0 +1,184 @@
+const burgerMenuIcon = document.getElementById("burgerMenuIcon");
+const closeMenu = document.getElementById("closeMenu");
+const menuOverlay = document.getElementById("menuBackgroundOverlay");
+const menuContainer = document.getElementById("menuContainer");
+const plusBtn = document.querySelector(".plus");
+const minusBtn = document.querySelector(".minus");
+const itemsToCartNumber = document.getElementById("numberOfItemsToCart");
+const addToCartBtn = document.getElementById("addToCart");
+const numberOfItemsInCart = document.getElementById("itemInCartAmount");
+const largeImgView = document.getElementById("productLargeView");
+const thumbnails = document.querySelectorAll(".thumbnails");
+const nxtImgIcon= document.querySelector(".nextIcon");
+const previousImgIcon= document.querySelector(".previousIcon");
+const itemBoughtInfoTxt = document.getElementById("itemBoughtInfoTxt");
+const totalAmountOfGoodsInCart = document.getElementById("totalAmountOfGoodsInCart");
+const timesItemsInCart = document.getElementById("timesItemsInCart");
+const binIcon = document.getElementById("binIcon");
+const checkoutBtn = document.getElementById("checkoutBtn");
+const itemToCheckoutInfo = document.getElementById("itemToCheckoutInfo");
+const cartIcon = document.getElementById("cartIcon");
+const cartedItemsHolder = document.querySelector(".hidee");
+const lightbox = document.getElementById("lightbox");
+const lightboxContents = document.getElementById("lightboxContents");
+const closeLightboxIcon= document.querySelector(".closeLightboxIcon");
+const emptyCartTxt = document.getElementById("emptyCartTxt");
+let defaultDigit = Number(itemsToCartNumber.innerHTML);
+let itemsInCart = Number(numberOfItemsInCart.innerHTML);
+const productsContainer= document.getElementById("productsContainer");
+const imagesFilePath = [	"images/image-product-1.jpg","images/image-product-2.jpg","images/image-product-3.jpg","images/image-product-4.jpg"];
+let indx = 1;
+
+
+//FUNCTION FOR NEXT IMAGE VIEW ON MOBILE.
+  function nxt(){
+  					indx = (indx + 1) % imagesFilePath.length;
+  			if(window.innerWidth<767){
+  						largeImgView.src= 	imagesFilePath[indx];
+  				}
+  		else	if(window.innerWidth>=767){	lightboxContents.children[1].children[0].children[0].src =	imagesFilePath[indx];
+  					}
+  			return imagesFilePath[indx];
+  }
+
+nxtImgIcon.addEventListener("click",nxt);
+
+
+//FUNCTION FOR PREVIOUS IMAGE VIEW ON MOBILE.
+  function previous(){
+    if(indx===0){
+      indx = imagesFilePath.length;
+    }
+    indx = indx-1;
+			if(window.innerWidth<767){
+						largeImgView.src=imagesFilePath[indx];
+			}
+		 else	if(window.innerWidth>=767){	lightboxContents.children[1].children[0].children[0].src =	imagesFilePath[indx];
+  					}
+  		return imagesFilePath[indx];
+  				
+  }
+previousImgIcon.addEventListener("click",previous);
+
+
+    //OPENS THE MOBILE MENU.
+		burgerMenuIcon.onclick=()=>{
+				menuOverlay.style.display="block";
+				menuContainer.style.width="60%";
+				}
+				
+				
+  //CLOSES THE MOBILE MENU.
+ 		closeMenu.onclick=()=>{
+			  	menuOverlay.style.display="none";
+					menuContainer.style.width="0%";
+				}
+							
+				  //TOGGLE CARTED ITEMS VIEW.
+     cartIcon.onclick=()=>{  
+          		
+cartedItemsHolder.classList.toggle("show")
+ 
+     }
+     
+				// INCREMENTS THE NUMBER OF ITEMS TO CART.				
+		plusBtn.onclick=()=>{
+  		 var increment = ++defaultDigit; 
+ itemsToCartNumber.innerHTML=increment;
+
+				}
+				
+				
+				// DECREMENTS THE NUMBER OF ITEMS TO CART.				
+		minusBtn.onclick=()=>{
+		if(defaultDigit>0){
+  		 let decrement = --defaultDigit; 
+ itemsToCartNumber.innerHTML=decrement;
+      }
+				}
+				
+				
+  //DELETES ITEMS IN THE CART.
+  binIcon.onclick=()=>{
+   	emptyCartTxt.style.display="block";
+				itemToCheckoutInfo.style.display="none";
+  		checkoutBtn.style.display="none";
+  			numberOfItemsInCart.style.display="none";
+			
+				totalAmountOfGoodsInCart.innerHTML=0; 
+  		timesItemsInCart.innerHTML=0;
+  	
+  }
+
+
+
+   // UPDATES THE NUMBER OF ITEMS IN CART.
+				addToCartBtn.onclick=()=>{
+				numberOfItemsInCart.style.display="block";
+				let itemsInCartNumber = itemsInCart+=defaultDigit;
+				 
+timesItemsInCart.innerHTML=itemsInCartNumber;
+let totalAmount = 125*itemsInCart;
+  			totalAmountOfGoodsInCart.innerHTML= ` \$${totalAmount}`;
+  			emptyCartTxt.style.display="none";
+  		itemToCheckoutInfo.style.display="flex";
+  		checkoutBtn.style.display="block";
+  		
+  	return	numberOfItemsInCart.innerHTML=itemsInCartNumber;
+  		
+  		
+				}
+				
+  
+  
+   //FOR THUMBNAILS DISPLAY ON LARGE VIEW.
+  for(i=0;i<thumbnails.length; i++){
+  thumbnails[i].onclick=(e)=>{
+  			largeImgView.src=e.target.src;
+  			}
+  }
+  
+  
+  
+  
+   
+   //FOR THE LIGHTBOX FUNCTIONS.
+    lightboxContents.onclick=(e)=>{
+    
+   if(e.target.alt==="product"){
+let chk =  lightboxContents.children[1].children[0].children[0].src=e.target.src;
+ }
+ 
+  else if(e.target.id==="nxtt"){
+    return nxt();			
+}
+
+else if(e.target.id==="prev"){
+				return previous();
+ }
+
+}  				
+    
+      
+ 
+  
+  //CHECKS IF THE WINDOW'S WIDTH IS ABOVE OR EQUALS TO 767PX, IF THEN, IT CHANGES THE APPLYS THE STATEMENT IN THE BLOCK.
+  if(window.innerWidth>=767){
+  	itemBoughtInfoTxt.innerHTML="Fall Limited Edition Sneakers";
+  	
+  				  //LIGHTBOX CLONED CREATION.
+		let cloneN = productsContainer.cloneNode(true);
+	lightboxContents.appendChild(cloneN);
+	
+     largeImgView.onclick=()=>{
+     lightboxContents.children[1].children[0].children[0].src=imagesFilePath[0];
+	lightbox.style.display="grid";			
+   }
+    closeLightboxIcon.onclick=()=>{
+    				lightbox.style.display="none";		
+    				}
+    
+  }
+        
+
+     
